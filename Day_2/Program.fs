71637b9b2336746f8f66d980b5692e223d2312f3
Christopher Boyle <chrismjboyle@gmail.com>
2025-12-02 21:19:35 +0000

    add solution for part2

--- Day_2/Program.fs
+++ Day_2/Program.fs
@@ -35,3 +35,35 @@ let part1 =
 
 printfn "Part 1: %A" part1
 
+// This is probably horrible... but it works!
+
+// Get the factors of a number in the greediest way possible
+let factors (i:int) = 
+  seq { 2 .. 1 .. i }
+  |> Seq.filter (fun d -> i % d = 0)
+
+// Split a string into some number of parts, as an array
+let into_parts (s: string) (n: int) =
+  let p = s.Length / n;
+  seq { 0 .. p .. (s.Length-1) }
+  |> Seq.map (fun i -> s[i..(i+p-1)])
+  |> Seq.toArray
+
+// Given an array of strings, check if they are all the same
+let all_equal (parts: string[]) =
+  parts
+  |> Seq.fold (fun a c -> a && (c = parts[0])) true
+
+// For a given integer - convert to string and check for runs of equal sequences
+let is_invalid_part2 (i: Int64) = 
+  let s = i.ToString();
+  let invalids = s.Length |> factors |> Seq.filter (fun d -> all_equal (into_parts s d) ) |> Seq.toArray;
+  invalids.Length > 0
+  
+// Sum invalid IDs (using part2 method)
+let part2 =
+  get_ranges input
+  |> Array.map (fun rng -> sum_invalid rng is_invalid_part2)
+  |> Array.fold (fun a b -> a + b) 0L
+
+printfn "Part 2: %A" part2
