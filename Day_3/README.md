a7158432894ca51899250511618b816ff275e0b0
Christopher Boyle <chrismjboyle@gmail.com>
2025-12-03 11:01:08 +0000

    add solution for day 3

--- Day_3/README.md
+++ Day_3/README.md
@@ -1,8 +1,14 @@
 # Day 3
 Let's `go`!
 
+Run by abusing an echoy cat:
+```bash
+echo part1 | cat - test.txt | go run .
+echo part2 | cat - test.txt | go run .
+```
+
 # Part 1
 Find the maximum value 2 digit integer that can be constructed from pairing two digits of a string together.
 
 # Part 2
-???
+Find the maximum value 12 digit integer that can be constructed by concatenating digits of a string together.
--- Day_3/day3.go
+++ Day_3/day3.go
@@ -22,17 +22,39 @@ func argmax(s string) (mxi, mxv int) {
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	line, _ := reader.ReadString('\n')
+
+	var num_batteries, partno int
+	switch line {
+	case "part1\n":
+		num_batteries = 2
+		partno = 1
+	case "part2\n":
+		num_batteries = 12
+		partno = 2
+	default:
+		panic("unexpected mode string!")
+	}
+
 	joltage := 0
 	for {
 		line, err := reader.ReadString('\n')
 		if (err != nil) { break }
 
-
+		// remove trailing newline
 		line = line[:len(line)-1]
-		i, tens := argmax(line[:len(line)-1])
-		_, units := argmax(line[i+1:])
-		joltage += tens * 10 + units
+		n := len(line)
+		i, j := 0, 0
+		for k := range num_batteries {
+			lb := i
+			ub := n - num_batteries + k + 1
+			di, v := argmax(line[lb:ub])
+			j *= 10
+			j += v
+			i = lb + di + 1
+		}
+		joltage += j
 		
 	}
-	fmt.Printf("%d\n", joltage)
+	fmt.Printf("part%d: %d\n", partno, joltage)
 }
